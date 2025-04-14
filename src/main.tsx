809b61e9dd5521cb140123afd81925bd68664de1
Anthocs <100413757+Anthocs@users.noreply.github.com>
2025-04-14 10:16:02 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TestDeTuring from './TestDeTuring';
+import TestDeTuring from './Neuronne';
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TestDeTuring />
+    <Neuronne />
   </React.StrictMode>
 );
